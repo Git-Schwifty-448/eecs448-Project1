958cec1750a221195b503665ef30580f42b5ef04
Abraham Dick <abedick@ku.edu>
2017-09-10 16:34:45 -0500

    time parse

--- src/user.rb
+++ src/user.rb
@@ -10,33 +10,46 @@
 class User
     
         def initialize
+            @user_name = String.new
             @drive = Driver.new
             @events = false
-            @militaryTime = false;
+            @military_time = true;
         end
 
         def run
 
             @events = true                              # only for testing
 
-            getEvents()
+            get_user_name()
+            get_events()
+
         end
 
-        def getEvents
+        # updates the member variable @user_name with a name given by the user
+        def get_user_name
+            @drive.title_print("User Mode")
+            @drive.sub_title_print("Get User Name")
+
+            print "Please enter a username: "
+            @user_name = gets.chomp
+        end
+
+
+        def get_events
             if(@events  == true) 
                 print "\n\n"
-                @drive.title_print("Events")
+                
 
                 # get the first event from the database
 
                 @name = "Event Sample"
                 @date = "10/9/17"
-                @time = ["3:30pm","4:00pm"]
+                @time = ["15:30","16:00"]
                 @attendees = "Alex, Q, Abe"
                 
                 send = [@name,@date,@time,@attendees]
 
-                eventTray(send)
+                event_tray(send)
 
 
 
@@ -46,22 +59,7 @@ class User
         end
 
         # outputs a single event as an array to send to eventTray
-        def singleEventPrinter
-
-        end
-
-        # outputs each event and returns if the user wants to attend
-        def eventTray(data)
-
-            @title = "Upcoming Events"
-
-            if(@militaryTime == false)
-                @title = @title + " (12hr)"
-            else 
-                @title = @title + " (24hr)"
-            end
-
-            @drive.sub_title_print(@title)
+        def single_event_printer(data)
 
             puts "\n Event Name: " + data[0]
             puts "       Date: " + data[1]
@@ -69,7 +67,29 @@ class User
             
             if data[2].kind_of?(Array)
                 for i in 0...data[2].length
-                    print data[2][i]
+
+                    # converter for standard time
+                    if !@military_time 
+                        
+                        @standard_time = String.new
+
+                        @temp_time = data[2][i].split(":")
+                        @temp_time[0] = @temp_time[0].to_i
+
+                        if @temp_time[0] > 12
+                            @temp_time[0] = @temp_time[0] - 12
+                            @temp_time[0] = @temp_time[0].to_s
+
+                            @standard_time = @temp_time[0] + ':' + @temp_time[1] + "pm"
+                        else
+                            @standard_time = @temp_time[0] + ':' + @temp_time[1] + "am"
+                        end
+
+                        print @standard_time
+                    else
+                        print data[2][i]
+                    end
+
                         if i != data[2].length-1
                             print ", "
                         end
@@ -80,6 +100,22 @@ class User
             end
 
             puts "  Attendees: " + data[3]
+        end
+
+        # outputs each event and returns if the user wants to attend
+        def event_tray(data)
+            @drive.title_print("Events")
+            @title = "Upcoming Events"
+
+            if(@militaryTime == false)
+                @title = @title + " (12hr)"
+            else 
+                @title = @title + " (24hr)"
+            end
+
+            @drive.sub_title_print(@title)
+
+            single_event_printer(data)
 
             print "\n    Toggle time format with t"
 
@@ -88,6 +124,21 @@ class User
             while (@input_char != 'y') && (@input_char != 'n') && (@input_char != 't')
                 print "\n\n    Do you wish to attend this event?: (y/n) "
                 @input_char = gets.chomp
+            end
+
+
+            case @input_char
+                when 'y'
+                when 'n'
+                when 't'
+                    #repring the event with military time flipped
+                    if !@military_time
+                        @military_time = true
+                    else
+                        @military_time = false
+                    end
+
+                    event_tray(data)
             end
 
         end
