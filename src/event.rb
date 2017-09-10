112fed29b8de41160b6ad2d6e5e104867c7c8bc0
QualenPollard <q152p288@ku.edu>
2017-09-10 14:49:24 -0500

    Accessor methods

--- src/event.rb
+++ src/event.rb
@@ -3,14 +3,37 @@
     File: event.rb
     Author:
     Date Created: 9/10/17
-    Description: 
+    Description:
 
 =end
 
 class Event
 
+    #Contstructor method
     def initialize
-
+      @name, @date, @time, @description
     end
+
+    #Accessor methods
+    def getName
+      @name
+    end
+
+    def getDate
+      @date
+    end
+
+    def getTime
+      @time
+    end
+
+    def getDescription
+      @description
+    end
+
+    
+
+
+
 
 end
