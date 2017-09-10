033475b396f735678ae107258d3b8e0e24862cb4
Abraham Dick <abedick@ku.edu>
2017-09-10 14:57:06 -0500

    event printer outline

--- src/driver.rb
+++ src/driver.rb
@@ -39,28 +39,47 @@ class Driver
         print "\u2518\n"
     end
 
+    def sub_title_print(title)
+        
+        title_size = title.length
+
+        # title line
+        print "\u2502   \u25B6 #{title} "
+        for i in 0...(@size - (title_size+6))
+            print " "
+        end
+        print "\u2502\n"
+
+        # bottom line
+        print "\u2514"
+        for i in 0...@size
+            print "\u2500"
+        end
+        print "\u2518\n"
+    end
+
     def menu_builder(option_array)
 
         
-                option_array.each do |option|            
-                    puts " \u21E8 #{option}"
-                end
+    option_array.each do |option|            
+        puts " \u21E8 #{option}"
+    end
+
+    menu_choice = choice_getter(1,option_array.length)
+    return menu_choice
+    
+    end
         
-                menu_choice = choice_getter(1,option_array.length)
-                return menu_choice
-                
-            end
+    def choice_getter(low,high)
         
-            def choice_getter(low,high)
-                
-                choice = 0
-        
-                while (choice < low || choice > high)
-                    print "Please make a selection: "
-                    choice = gets.to_i
-                end
-        
-                return choice
-        
-            end
+        choice = 0
+
+        while (choice < low || choice > high)
+            print "Please make a selection: "
+            choice = gets.to_i
+        end
+
+        return choice
+
+    end
 end
--- src/user.rb
+++ src/user.rb
@@ -10,9 +10,60 @@
 class User
     
         def initialize
+            @drive = Driver.new
+            @events = false
+            @militaryTime = false;
         end
 
         def run
-            puts "user"
+
+            @events = true                              # only for testing
+
+            getEvents()
         end
+
+        def getEvents
+            if(@events  == true) 
+                print "\n\n"
+                @drive.title_print("Events")
+
+                # get the first event from the database
+
+                name = "Event Sample"
+                date = "10/9/17"
+                time = ["3:30pm","4:00pm"]
+                attendees = "Alex, Q, Abe"
+                
+                eventTray(name,date,time,attendees)
+
+
+
+            else
+                print "There are not currently any events in the database\n"
+            end
+        end
+
+        # outputs a single event as an array to send to eventTray
+        def singleEvent
+
+        end
+
+        # outputs each event and returns if the user wants to attend
+        def eventTray(name,date,time,attendees)
+
+            @title = "Upcoming Events"
+
+            if(@militaryTime == false)
+                @title = @title + " (12hr)"
+            else 
+                @title = @title + " (24hr)"
+            end
+            
+            @title = @title + " (Toggle with t)"
+
+            @drive.sub_title_print(@title)
+
+
+        end
+            
     end
