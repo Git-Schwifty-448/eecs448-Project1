71578e5886e996ea259a89b551f0bfd2cb629cbe
QualenPollard <q152p288@ku.edu>
2017-09-10 17:50:48 -0500

    Admin info

--- src/admin.rb
+++ src/admin.rb
@@ -3,17 +3,61 @@
     File: admin.rb
     Author:
     Date Created: 9/8/17
-    Description: 
+    Description:
 
 =end
 
 class Admin
-    
+
         def initialize
+          @fName, @lName = " ", " "
         end
-    
+
         def run
-            puts "admin"
+          print getAdminInfo
+        end
+
+        #Accessor method
+        def getFName
+          @fName
+        end
+
+        def getLName
+          @lName
+        end
+
+        #Setter method
+        def setFName(theFName)
+          @fName = theFName;
+        end
+
+        def setLName(theLName)
+          @lName = theLName
+        end
+
+        #Receive admin's information
+        def getAdminInfo
+          print "Enter your First Name: "
+          firstName = gets.chomp
+
+          #Using Regexp class
+          while !(firstName =~ /[[:alpha:]]/)
+            print "Not a valid input. Enter your First Name: "
+            firstName = gets.chomp
+          end
+
+          print "Enter your Last Name: "
+          lastName = gets.chomp
+
+          #Using Regexp class
+          while !(lastName =~ /[[:alpha:]]/)
+            print "Not a valid input. Enter your Last Name: "
+            lastName = gets.chomp
+          end
+
+          setFName(firstName)
+          setLName(lastName)
+
         end
 
     end
