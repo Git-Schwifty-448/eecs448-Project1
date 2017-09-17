7aa016d787d05e9f2a6a3d809b2a764767b521e4
Alex Shadley <shadleyalex@gmail.com>
2017-09-17 14:09:03 -0500

    fixes DatabaseController, fixes attendee indentation

--- src/controller.rb
+++ src/controller.rb
@@ -50,13 +50,13 @@ class Controller
                 @user_controller.run
         end
 
-		dbCont = DatabaseController.new()
-		e = Event.new('event', 'description', [DateTime.new(2017, 9, 15, 10), DateTime.new(2017, 9, 15, 9), DateTime.new(2017, 9, 15, 11)], [])
-		a = Attendee.new('Mike', [DateTime.new(2017, 9, 15, 10)])
-		e.add_attendee(a)
+		#dbCont = DatabaseController.new()
+		#e = Event.new('event', 'description', [DateTime.new(2017, 9, 15, 10), DateTime.new(2017, 9, 15, 9), DateTime.new(2017, 9, 15, 11)], [])
+		#a = Attendee.new('Mike', [DateTime.new(2017, 9, 15, 10)])
+		#e.add_attendee(a)
 		#dbCont.persist_event(e)
 
-		puts e.get_date
+		#sputs dbCont.get_events()[0].get_timeslots[0].hour
     end
 
 end
